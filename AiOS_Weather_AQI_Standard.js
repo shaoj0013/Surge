3eda1ffa641bd70c4ceb4d4cac17a55669b03353
shaoj0013 <shaoj0013@gmail.com>
2020-12-28 11:31:51 +0800

    Update AiOS_Weather_AQI_Standard.js

--- AiOS_Weather_AQI_Standard.js
+++ AiOS_Weather_AQI_Standard.js
@@ -6,7 +6,7 @@ const aqicnToken = 'a501c448f5e1bb65745a2b7c772eccd3dd2a9b93'
 // STEP 2: 参考下方配置片段，在代理工具的配置文件中添加对应的配置。注意：script-path 后应该替换为添加 apicnToken 值后的脚本路径
 /*
 	[Script]
-	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=https://raw.githubusercontent.com/shaoj0013/Surge/main/iOS_Weather_AQI_Standard.js
+	AQI-US = type=http-response, pattern=https://weather-data.apple.com/v1/weather/[\w-]+/[0-9]+\.[0-9]+/[0-9]+\.[0-9]+\?include=, requires-body=true, script-path=https://raw.githubusercontent.com/shaoj0013/Surge/main/AiOS_Weather_AQI_Standard.js
 
 	[MITM]
 	hostname = weather-data.apple.com
